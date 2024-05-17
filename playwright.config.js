4674985fd846c2dc1dccdf82654dac1b2bc9c37e
Sami Sallmén <sami.sallmen@qubilea.fi>
2024-05-17 17:09:15 +0300

    Take Allure Reports into use

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test')
+const { testPlanFilter } = require('allure-playwright/dist/testplan')
 
 /**
  * Read environment variables from file.
@@ -20,8 +21,9 @@ module.exports = defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  grep: testPlanFilter(),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
